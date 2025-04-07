4ecb07b345eb0dfcb9d2dfd45eac64faf4ddee38
Seungjun <rkdtmdwns0105@gmail.com>
2025-04-07 22:22:21 +0900

    250407

---
+++ typedef.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+//typedef std::vector<std::pair<std::string, int>> pairlist_t;
+typedef std::string text_t;
+
+int main(){
+
+    text_t firstName = "John";
+    std::cout << firstName << '\n';
+
+    return 0;
+}
